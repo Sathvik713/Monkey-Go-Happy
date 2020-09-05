66bb294d2bae8526c76392c2f20321974c20b29f
Sathvik713 <70425727+Sathvik713@users.noreply.github.com>
2020-09-05 13:07:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var bananaImage;
+var obstacleImage, obstacleGroup;
+var background;
+
+var score;
+
+function preload()
+background = Image("jungle.jpg");
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
